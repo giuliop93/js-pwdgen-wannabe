8f91e625c9ac7f7f0a096696beceaa8b05a1891b
Giulio_Pa <giuliop93@hotmail.it>
2021-04-07 14:58:02 +0200

    wip

---
+++ script.js
@@ -0,0 +1,14 @@
+// Chiedo all’utente il suo nome
+var nomeUtente = prompt('Qual è il tuo nome?');
+// chiedo il suo cognome
+var cognomeUtente = prompt('Qual è il tuo cognome?');
+// chiedo il suo colore preferito
+var colorePreferito = prompt('Qual è il tuo colore preferito?');
+
+var password = nomeUtente + cognomeUtente + colorePreferito + "21";
+// scrivo sulla pagina la password generata
+document.getElementById("password-generata").innerHTML = password;
+// scrivo sulla pagina tutti i dati di input raccolti
+document.getElementById('nome-utente').innerHTML = nomeUtente;
+document.getElementById('cognome-utente').innerHTML = cognomeUtente;
+document.getElementById('colore-preferito').innerHTML = colorePreferito;
